028b42842431ec6f072180cf468aeaaec2961f76
Andy <qanh27@gmail.com>
2015-07-25 13:29:01 +0800

    added libs

--- heart-a-tech/www/js/app.js
+++ heart-a-tech/www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+var app = angular.module('starter', ['ionic', 'starter.controllers','ionic-material', 'ionMdInput', 'starter.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -82,4 +82,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/tab/dash');
 
+
 });
+
+
